3dcea7beefc4d647464fa2ebaa8bc99d378f4ab9
Md Minhaj <parsonal322532@gmail.com>
2024-01-09 16:20:39 +0600

    About Us Section Complete

--- src/Components/About_Us/About_Us.jsx
+++ src/Components/About_Us/About_Us.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import parsonal from '../../assets/images/about_us/person.jpg';
+import parse from '../../assets/images/about_us/parts.jpg';
 
 const About_Us = () => {
     return (
-        <div>
-            
+        <div className="grid lg:grid-cols-2 gap-10 pt-2 pb-12">
+            <div className='relative w-full'>
+                <div className='w-[90%] h-[90%] lg:h-[75%] xl:h-[90%]'>
+                    <img className='rounded-lg w-full h-full' src={parsonal} alt="" />
+                </div>
+                <div className='absolute bottom-0 right-0 w-[50%] lg:w-[75%] xl:w-[50%]'>
+                    <img className='rounded-lg border-8 w-full border-white' src={parse} alt="" />
+                </div>
+            </div>
+            <div className='text-center md:text-start'>
+                <h2 className="text-orange-600 text-xl font-bold font-['Inter']">About Us</h2>
+                <h1 className="text-neutral-900 text-[30px] md:text-[45px] font-bold font-['Inter']">We are qualified & of experience in this field</h1>
+                <p className="text-neutral-500 text-base font-normal font-['Inter'] capitalize md:leading-[30px] mt-4">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <p className="text-neutral-500 my-4 text-base font-normal font-['Inter'] capitalize leading-[30px]">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <button className="btn btn-error text-center mt-2 text-white text-lg font-semibold font-['Inter']">Get More Info</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../../Components/Footer/Footer';
 import Carousel from '../../Components/Carousel/Carousel';
+import About_Us from '../../Components/About_Us/About_Us';
 
 const Home = () => {
     return (
         <div className='container mx-auto'>
             <Carousel></Carousel>
+            <About_Us></About_Us>
             <Footer></Footer>
         </div>
     );
